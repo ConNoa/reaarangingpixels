b367fa34b20978a5ff097be2c60b05643630cc38
ConNoa <constantin.noack@uni-weimar.de>
2019-12-18 15:38:51 +0100

    daa

---
+++ source/collmap.hpp
@@ -0,0 +1,28 @@
+#ifndef COLLMAP_HPP
+#define COLLMAP_HPP
+
+#include <vector>
+
+class Collmap{
+public:
+  Collmap();
+  Collmap(int width, int height);
+
+
+
+private:
+  int _w;
+  int _h;
+  std::vector<std::vector<int>>(_colisio);
+  //, std::vector<int> (int, int));
+
+
+};
+
+
+
+
+
+
+
+#endif
